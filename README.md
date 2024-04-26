125c3e93f05f317c8e68c40269003b0d25947601
julio orozco <julio11orozco09gmail.com>
2024-04-25 21:52:18 -0600

    nueva funcion

---
+++ README.md
@@ -0,0 +1,26 @@
+Predecir El Tipo de Pokemon
+Las variables que utilizamos son :
+* hp: Puntos de Vida ////estadísticas de salud del Pokemon
+* attack: el valor de atáque Pokemon
+* defense: el valor de Defense of the Pokemon
+* sp_attack: valor de ataque especial Pokemon
+* sp_defense: valor de defensa especial
+* speed: valor base de la velocidad
+* generation: The numbered generation which the Pokemon was first introduced
+• Objetivo del proyecto de machine learning
+* Desarrollar una API de predicción de tipos de Pokémon utilizando técnicas de aprendizaje automático. Utilizaremos el conjunto de datos de Pokémon de Kaggle, que contiene información detallada sobre los personajes de Pokémon, incluidos sus atributos, tipos y habilidades. El objetivo es que la API pueda determinar el tipo de un Pokémon en función de sus características. • Definición si es machine learning o deep learning
+* Machine Learning • Tecnologías a utilizar (esto porque algunos pueden utilizar tecnologías diferentes al curso sin ningún problema, pero es importante tenerlas mapeadas)
+* DataSpell
+* Pycharm
+* Bibliotecas de machine learning: scikit-learn, TensorFlow o PyTorch
+* Framework de desarrollo de API: Flask o FastAPI
+* Base de datos: CSV
+* GitHub -Docker /Dockerhub
+* AWS • Diseño y diagrama de la arquitectura a utilizar Roles de cada estudiante dentro del proyecto *-Estudiante 1 (Científico de Datos):
+* Análisis exploratorio de datos.
+* Preprocesamiento y limpieza de datos.
+* Entrenamiento y evaluación de modelos de machine learning.
+* Documentación técnica y reportes. *Estudiante 2 (MLOps y Seguridad e Infraestructura):
+* Desarrollo de la API utilizando Flask o FastAPI.
+* Configuración de la base de datos y su integración con la API.
+* Implementación de Docker para contenerización.
--- main.py
+++ main.py
@@ -1,29 +1,38 @@
-from fastapi import FastAPI, status, HTTPException
-from fastapi.responses import JSONResponse
+
+from fastapi import FastAPI, HTTPException, status
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 import pandas as pd
 import joblib
+import json
+import random
 
 app = FastAPI(
     title="Pokémon Type Prediction API",
-    version="0.0.1"
+    version="0.0.2"
 )
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Cargar el modelo entrenado
 model = joblib.load("model/logistic_regression_tipo_pokemon.pkl")
 
+# Cargar los datos de los Pokémon desde un archivo JSON
+with open("data.json", "r") as file:
+    pokemon_data = json.load(file)
 
 @app.post("/api/v1/predict-pokemon-type", tags=["pokemon-type-prediction"])
-async def predict(
-        hp: float,
-        attack: float,
-        defense: float,
-        sp_attack: float,
-        sp_defense: float,
-        speed: float,
-        generation: float
+async def predict_pokemon_type(
+    hp: float,
+    attack: float,
+    defense: float,
+    sp_attack: float,
+    sp_defense: float,
+    speed: float,
+    generation: float
 ):
     try:
-        # Crear el DataFrame directamente desde el diccionario
+        # Crear el DataFrame directamente desde los parámetros recibidos
         data_frame = pd.DataFrame({
             'hp': [hp],
             'attack': [attack],
@@ -34,23 +43,39 @@ async def predict(
             'generation': [generation]
         })
 
-        # Realizar la predicción
+        # Realizar la predicción del tipo de Pokémon
         prediction = model.predict(data_frame)
-        predicted_type = prediction[0]  # Suponiendo que la predicción es un número que representa el tipo
+        predicted_type = prediction[0]
 
-        # Devolver la predicción como JSON
-        return JSONResponse(
-            status_code=status.HTTP_200_OK,
-            content={"predicted_type": predicted_type}  # Devolver el tipo predicho dentro de un diccionario
-        )
+        # Buscar la imagen asociada al tipo predicho en el archivo JSON
+        predicted_pokemon = next((pokemon for pokemon in pokemon_data if pokemon["predicted_type"] == predicted_type), None)
+        if predicted_pokemon is None:
+            raise HTTPException(
+                detail=f"No se encontró una imagen asociada con el tipo de Pokémon predicho: {predicted_type}",
+                status_code=status.HTTP_404_NOT_FOUND
+            )
+
+        # Construir la respuesta HTML con la imagen asociada al tipo predicho
+        html_content = f"""
+        <html>
+        <head>
+            <title>Pokémon Prediction</title>
+            <link rel="stylesheet" href="/static/styles.css">
+        </head>
+        <body>
+            <h1>Pokémon Type Prediction</h1>
+            <p>The predicted type is: {predicted_type}</p>
+            <img src="{predicted_pokemon['pokemon_image_url']}" alt="{predicted_type}">
+        </body>
+        </html>
+        """
+
+        return HTMLResponse(content=html_content, status_code=status.HTTP_200_OK)
     except Exception as e:
-        # Manejar los errores
         raise HTTPException(
             detail=str(e),
             status_code=status.HTTP_400_BAD_REQUEST
         )
-
-
 
 
 
